c82d961537a4107d79bbcfd78c4f7641d8da2563
longmaker2 <l.deng@alustudent.com>
2023-11-30 15:30:48 +0200

    updated files

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://localhost:4000/api/v1";
+// export const BASE_URL = "http://localhost:4000/api/v1";
+export const BASE_URL =
+  "https://eu-west-1.aws.data.mongodb-api.com/app/data-pocwx/endpoint/data/v1";
