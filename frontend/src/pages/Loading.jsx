afdb48b6d712d24dc075515270a8b2bde8e8f090
longmaker2 <l.deng@alustudent.com>
2023-12-01 20:07:53 +0200

    Updated the loading effect

--- frontend/src/pages/Loading.jsx
+++ frontend/src/pages/Loading.jsx
@@ -5,7 +5,7 @@ import { RingLoader } from "react-spinners";
 const Loading = () => {
   return (
     <div className="loading-container">
-      <RingLoader color={"#1a5d1a"} loading={true} size={150} />
+      <RingLoader color={"#3bb873"} loading={true} size={150} />
     </div>
   );
 };
