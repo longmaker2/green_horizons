42479fdc99e145a0d0ecd34a8e501d3daa884f70
longmaker2 <l.deng@alustudent.com>
2023-12-02 20:28:24 +0200

    updated the about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../styles/about.css";
 import Loading from "./Loading";
-
 import { Container, Row, Col } from "reactstrap";
 
 const About = () => {
@@ -21,28 +20,37 @@ const About = () => {
       {loading ? (
         <Loading />
       ) : (
-        <>
-          <section>
-            <Container>
-              <Row>
-                <Col>
-                  <div className="about-container">
-                    <h1>About Us</h1>
-                    <p>
-                      Welcome to Green Horizons, your go-to platform for
-                      exciting and eco-friendly tours around the world.
-                    </p>
-                    <p>
-                      Our mission is to provide unforgettable experiences while
-                      promoting sustainable travel and environmental
-                      consciousness.
-                    </p>
-                  </div>
-                </Col>
-              </Row>
-            </Container>
-          </section>
-        </>
+        <section>
+          <Container className="about-container">
+            <Row>
+              <Col>
+                <div className="about__text">
+                  <h1 className="text-center">About Green Horizons</h1>
+                  <p>
+                    Welcome to Green Horizons, your go-to platform for exciting
+                    and eco-friendly tours around South Sudan. Our mission is to
+                    provide unforgettable experiences while promoting
+                    sustainable travel and environmental consciousness.
+                  </p>
+                  <p>
+                    At Green Horizons, we believe in unlocking the untapped
+                    tourism potential of South Sudan through the "Green
+                    Horizons: South Sudan's Tourism & Wildlife Sanctuary"
+                    project. Our goal is not only to address the challenges of
+                    inadequate tourism infrastructure but also to combat the
+                    underrepresentation of conservation efforts online.
+                  </p>
+                  <p>
+                    Our one-stop online platform is designed to enhance tourist
+                    engagement, raise awareness about conservation, foster
+                    collaboration with local communities, and stimulate economic
+                    growth through responsible tourism.
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
       )}
     </>
   );
