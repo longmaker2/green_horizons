7b03987536e86145abe2760fe7707ccfd37a3c30
longmaker2 <l.deng@alustudent.com>
2023-12-02 17:24:29 +0200

    updated the about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -21,26 +21,28 @@ const About = () => {
       {loading ? (
         <Loading />
       ) : (
-        <section>
-          <Container>
-            <Row>
-              <Col>
-                <div className="about-container">
-                  <h1>About Us</h1>
-                  <p>
-                    Welcome to Green Horizons, your go-to platform for exciting
-                    and eco-friendly tours around the world.
-                  </p>
-                  <p>
-                    Our mission is to provide unforgettable experiences while
-                    promoting sustainable travel and environmental
-                    consciousness.
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </section>
+        <>
+          <section>
+            <Container>
+              <Row>
+                <Col>
+                  <div className="about-container">
+                    <h1>About Us</h1>
+                    <p>
+                      Welcome to Green Horizons, your go-to platform for
+                      exciting and eco-friendly tours around the world.
+                    </p>
+                    <p>
+                      Our mission is to provide unforgettable experiences while
+                      promoting sustainable travel and environmental
+                      consciousness.
+                    </p>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </>
       )}
     </>
   );
