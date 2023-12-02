fd1790afde79b64c9b77fde7656055ff0e0b6086
longmaker2 <l.deng@alustudent.com>
2023-12-02 02:11:28 +0200

    deployed on tech domain

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-  origin: ["https://long-green-horizons.vercel.app", "www.longmaker.tech"],
+  origin: "www.longmaker.tech",
   credentials: true,
 };
 
