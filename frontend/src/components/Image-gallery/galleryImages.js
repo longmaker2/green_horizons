25622fb49ff58763b461a2959bfbf5e1ccbf35a9
longmaker2 <l.deng@alustudent.com>
2023-12-04 03:10:55 +0200

    updated the gallery

--- frontend/src/components/Image-gallery/galleryImages.js
+++ frontend/src/components/Image-gallery/galleryImages.js
@@ -5,16 +5,25 @@ import galleryImg04 from "../../assets/images/gallery-04.jpg";
 import galleryImg05 from "../../assets/images/gallery-05.jpg";
 import galleryImg06 from "../../assets/images/gallery-06.jpg";
 import galleryImg07 from "../../assets/images/gallery-07.jpg";
+import gallaryImg08 from "../../assets/images/gallery-08.jpg";
+import gallaryImg09 from "../../assets/images/gallery-09.jpg";
+import gallaryImg10 from "../../assets/images/gallery-10.jpg";
+import gallaryImg11 from "../../assets/images/gallery-11.jpg";
+import gallaryImg12 from "../../assets/images/gallery-12.jpg";
 
 const galleryImages = [
   galleryImg01,
-  galleryImg07,
   galleryImg02,
   galleryImg06,
   galleryImg03,
   galleryImg04,
   galleryImg05,
   galleryImg07,
+  gallaryImg08,
+  gallaryImg09,
+  gallaryImg10,
+  gallaryImg11,
+  gallaryImg12,
 ];
 
 export default galleryImages;
